29f8cb3d331f032681b63f1143cbcde6048ec8bc
Nadeem Fayaz <iamabnadeem99@gmail.com>
2024-04-30 18:12:16 +0530

    FIx

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset', "module:react-native-dotenv"],
+  presets: ['module:@react-native/babel-preset'],
   plugins: [
     'react-native-reanimated/plugin'
   ]
